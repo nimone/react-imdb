3d32ddc762a8abba3321b8af38ecd81340dfcaba
nimone <nishantmogha1234@gmail.com>
2021-08-23 12:29:28 +0530

    display ratings on title card

--- src/components/TitleList.jsx
+++ src/components/TitleList.jsx
@@ -21,9 +21,12 @@ export default function TitleList({ header, titles, loadMore, hasMore }) {
       {titles.map((t, i) => 
         <div 
           ref={(titles.length === i + 1) ? lastTitleRef : null}
-          className="m-2 w-44 hover:opacity-90" 
+          className="relative m-2 w-44 hover:opacity-90" 
           key={t.id}
         >
+          <div className="flex justify-center items-center w-8 h-8 absolute top-0 right-0 p-1 m-1 z-2 text-sm font-bold rounded-full bg-dark-800/70 text-white">
+            {t.vote_average ? t.vote_average : "N/A"}
+          </div>
           <Link to={`/title/${t.media_type}/${t.id}`}>
             <Card
               id={t.id} 
